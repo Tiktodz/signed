8bff5a070992f57fe45973bc58fcda0b0701a043
Meisie Widuri <ewprjkt@proton.me>
2023-11-20 02:22:37 +0700

    Create signing.sh

---
+++ signing.sh
@@ -0,0 +1,46 @@
+function keygen() {
+    [ -z "$1" ] || certs_dir=$1
+    rm -rf "$certs_dir"
+    mkdir -p "$certs_dir"
+    local subject
+    echo "Sample subject: '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'"
+    echo "Now enter subject details for your keys:"
+    for entry in C ST L O OU CN emailAddress; do
+        echo -n "$entry:"
+        read -r val
+        subject+="/$entry=$val"
+    done
+    for cert in bluetooth cyngn-app media networkstack platform releasekey sdk_sandbox shared testcert testkey verity; do \
+        ./development/tools/make_key ~/.android-certs/$cert "$subject"; \
+    done
+}
+
+# make the commands runnable
+function cmd() {
+. build/envsetup.sh
+breakfast X00T
+mka sign_target_files_apks && mka ota_from_target_files otatools
+
+}
+
+# signing all apks!
+function sign() {
+croot
+sign_target_files_apks -o -d ~/.android-certs \
+    $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
+    signed-target_files.zip
+
+}
+
+# installable zip!
+function zip() {
+ota_from_target_files -k ~/.android-certs/releasekey \
+    --block --backup=true \
+    signed-target_files.zip \
+    signed-ota_update.zip
+
+}
+keygen
+cmd
+sign
+zip
