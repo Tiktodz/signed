6bbe118bd3085849048031a046d73d9f5c0ef1f6
dotkit <ewprjkt@proton.me>
2024-05-30 08:43:45 +0000

    Regenerate
    Change-Id: I4b7265d71727ca3e23c078ddb44f8949f9d7f962

--- generate_key.sh
+++ generate_key.sh
@@ -12,18 +12,9 @@ function keygen() {
         subject+="/$entry=$val"
     done
 
-    for cert in bluetooth media networkstack platform releasekey sdk_sandbox shared testcert testkey verity; do \
+    for cert in bluetooth media networkstack otakey platform releasekey sdk_sandbox shared testkey verity; do \
         ./development/tools/make_key ~/.android-certs/$cert "$subject"; \
     done
-
-    cp ./development/tools/make_key ~/.android-certs/
-    sed -i 's|2048|4096|g' ~/.android-certs/make_key
-
-    for apex in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.art com.android.bluetooth com.android.btservices com.android.cellbroadcast com.android.compos com.android.connectivity.resources com.android.conscrypt com.android.extservices com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.resolv com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.tethering com.android.tzdata com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.qorvo.uwb; do \
-    subject='/C=AS/ST=Indonesa/L=Jakarta View/O=Android/OU=Android/CN='$apex'/emailAddress=Android@Android.com'; \
-    ~/.android-certs/make_key ~/.android-certs/$apex "$subject"; \
-    openssl pkey -in ~/.android-certs/$apex.pk8 -inform DER -out ~/.android-certs/$apex.pem; \
-done
 }
 
 keygen
