09f7f9023b21ad57216994b78ee43ebb4b2809eb
Meisie Widuri <ewprjkt@proton.me>
2024-07-02 18:48:53 +0700

    Update generate_key.sh

--- generate_key.sh
+++ generate_key.sh
@@ -12,7 +12,7 @@ function keygen() {
         subject+="/$entry=$val"
     done
 
-for cert in bluetooth media networkstack platform releasekey sdk_sandbox shared testcert testkey verity; do \
+for cert in bluetooth cyngn-app media networkstack nfc platform releasekey sdk_sandbox shared testcert testkey verity; do \
         ./development/tools/make_key ~/.android-certs/$cert "$subject"; \
     done
 }
